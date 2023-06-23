e173a92156999f1460398c3c644d57e7926ed310
VoVyKhang <vo.vykhang24@gmail.com>
2023-06-23 21:08:39 +0700

    [ADD]: Hero section responsive

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -6,14 +6,24 @@ import avatar from '../../images/avatar.png'
 
 export function HeroSection() {
   return (
-    <Box component="section" pt={18} pb={9}>
+    <Box component="section" pt={{ xs: 4, md: 18 }} pb={{ xs: 7, md: 9 }}>
       <Container>
-        <Stack direction="row" justifyContent="flex-start" spacing={8}>
+        <Stack
+          direction={{ xs: 'column-reverse', md: 'row' }}
+          alignItems={{ xs: 'center', md: 'flex-start' }}
+          textAlign={{ xs: 'center', md: 'left' }}
+          spacing={8}
+        >
           <Box>
-            <Typography component="h1" variant="h3" fontWeight="bold" mb={5}>
+            <Typography
+              component="h1"
+              variant="h3"
+              fontWeight="bold"
+              mb={{ xs: 3.5, md: 5 }}
+            >
               Hi, I am John, <br /> Creative Technologist
             </Typography>
-            <Typography mb={5}>
+            <Typography mb={{ xs: 3.5, md: 5 }}>
               Amet minim mollit non deserunt ullamco est sit aliqua dolor do
               amet sint. Velit officia consequat duis enim velit mollit.
               Exercitation veniam consequat sunt nostrud
@@ -25,6 +35,7 @@ export function HeroSection() {
           <Box
             sx={{
               minWidth: '240px',
+              width: '100px',
               boxShadow: '-8px 10px',
               color: 'secondary.light',
               borderRadius: '50%',
