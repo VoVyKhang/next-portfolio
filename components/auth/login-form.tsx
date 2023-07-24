a03f6c3f2cb69849622bcc7468b25f3a43b28cc3
VoVyKhang <vo.vykhang24@gmail.com>
2023-07-24 15:29:55 +0700

    [ADD]: Setting input field

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -2,11 +2,12 @@ import { Box } from '@mui/system'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import InputField from '../form/input-field'
+import { Button } from '@mui/material'
 
 export function LoginForm() {
   const { control, handleSubmit } = useForm({
     defaultValues: {
-      username: 1,
+      username: '',
       password: '',
     },
   })
@@ -16,8 +17,17 @@ export function LoginForm() {
   }
 
   return (
-    <Box component={'form'} onSubmit={() => handleSubmit(handleLoginSubmit)}>
-      <InputField name="username" label="hjaha" control={control} />
+    <Box component={'form'} onSubmit={handleSubmit(handleLoginSubmit)}>
+      <InputField name="username" label="username" control={control} />
+      <InputField
+        type="password"
+        name="password"
+        label="password"
+        control={control}
+      />
+      <Button variant="contained" type="submit">
+        Login
+      </Button>
     </Box>
   )
 }
--- components/form/input-field.tsx
+++ components/form/input-field.tsx
@@ -1,14 +1,23 @@
+import { TextField, TextFieldProps } from '@mui/material'
 import { Box } from '@mui/system'
 import * as React from 'react'
 import { Control, useController } from 'react-hook-form'
 
-interface InputFieldProps {
+export type InputFieldProps = TextFieldProps & {
   name: string
-  label?: string
   control: Control<any>
 }
 
-export default function InputField({ name, label, control }: InputFieldProps) {
+export default function InputField({
+  name,
+  label,
+  control,
+  onChange: externalOnChange,
+  onBlur: externalOnBlur,
+  ref: externalRef,
+  value: externalValue,
+  ...rest
+}: InputFieldProps) {
   const {
     field: { onBlur, onChange, ref, value },
     fieldState: { error },
@@ -18,8 +27,16 @@ export default function InputField({ name, label, control }: InputFieldProps) {
   })
 
   return (
-    <Box onClick={() => onChange(value + 1)} ref={ref}>
-      {name}: {value}
-    </Box>
+    <TextField
+      fullWidth
+      size="small"
+      margin="normal"
+      name={name}
+      value={value}
+      onChange={onChange}
+      inputRef={ref}
+      onBlur={onBlur}
+      {...rest}
+    />
   )
 }
